cfb72de082d0ebc7105101aa993fc1c32065b624
Mayowa Makinde <mayowamakinde23@gmail.com>
2026-05-03 18:06:27 +0100

    chore: upgrade eslint-config-next to v16.1.3 and update eslint configuration dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,39 @@
-import { defineConfig, globalIgnores } from "@eslint/config";
+// import { FlatCompat } from "@eslint/eslintrc";
+
+// const compat = new FlatCompat({
+//   baseDirectory: import.meta.dirname,
+// });
+
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+//   {
+//     ignores: [
+//       ".next/**",
+//       "out/**",
+//       "build/**",
+//       "next-env.d.ts",
+//     ],
+//   },
+// ];
+
+// export default eslintConfig;
+
+import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+
+  // {
+  //   rules: {
+  //     "@typescript-eslint/no-explicit-any": "off",
+  //     "react/no-unescaped-entities": "off",
+  //   },
+  // },
+
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
 
 export default eslintConfig;
